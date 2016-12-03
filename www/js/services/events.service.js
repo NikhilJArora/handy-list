c7bf451904326a8cb6c45c64707690736cfe731b
NikhilJArora <nikhiljarora@outlook.com>
2016-12-03 14:47:29 -0500

    Added functionality to the events service

--- www/js/services/events.service.js
+++ www/js/services/events.service.js
@@ -7,18 +7,18 @@ angular.module('events')
 
 function eventsFactory($log) {
 
-  var eventsObj = {};
-  var eventsList = [
-      {eventId: 1, title: 'Backyard BBQ', description: 'event will be held in the backyard', date:'2016-12-22'},
-      {eventId: 2, title: 'Family Gathering', description: 'event will be held in the backyard', date:'2016-12-25'}
+
+  var _eventsList = [
+      {title: 'Backyard BBQ', description: 'event will be held in the backyard', date:'2016-12-22'},
+      {title: 'Family Gathering', description: 'event will be held in the backyard', date:'2016-12-25'}
   ];
 
 
 //Public API//
   var service = {
     getEvents : getEvents,
-    deleteEvent : deleteEvent,
-    addEvent : addEvent
+    addEvent : addEvent,
+    deleteEvent : deleteEvent
   };
 
   return service;
@@ -28,22 +28,44 @@ function eventsFactory($log) {
 
 //returns the minimal list used to build the list view for user to choose what event they want to view
 function getEvents() {
-  return eventsList;
+  return _eventsList;
 }
 
 //returns the full event with all detailed information (used for the event page )
 function getEventDetailed(name) {
-
+  //TODO
 }
 
 //removes event from list...
-function deleteEvent() {
+function deleteEvent(index) {
+  if (index > -1){
+      _eventsList.splice(index, 1);
+  }
 
 }
 
 //will add new event to list and create an object for that event (for now, can store in flat files... eventually will use mongoDB)
-function addEvent() {
+function addEvent(event) {
+  {
 
+    if(validEventFormat(event)){
+
+    }
+  }
+}
+
+
+//checks to see event structure
+function validEventFormat(event){
+  if(event && event.title && event.description){
+    var unique = _eventsList.find(function(element) {
+      return (element.title === this.trim());
+    }, event.title);
+    if (unique === undefined){
+      return true;
+    }
+  }
+  return false;
 }
 
 
